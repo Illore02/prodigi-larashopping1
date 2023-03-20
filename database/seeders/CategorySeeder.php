eccd83d038b93a8783e79eafaa7efb8caac4bcad
Lorenzo <lorenzosalvadori02@gmail.com>
2023-03-20 10:40:21 +0100

    fill CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -7,11 +7,32 @@ use Illuminate\Database\Seeder;
 
 class CategorySeeder extends Seeder
 {
+    private $categories = [
+        [
+            "id" => 1,
+            "name" => "Uomo"
+        ],
+        [
+            "id" => 2,
+            "name" => "Donna"
+        ],
+        [
+            "id" => 3,
+            "name" => "Bambino"
+        ],
+    ];    
+    
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        foreach ($this->categories as $category)
+        {
+            Category::create([
+                "name" => $category['name']
+            ]);
+        }
     }
 }
+
